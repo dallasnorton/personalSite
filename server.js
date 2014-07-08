c80ab8662ac4c4f8c134c8dc2828133a0b6d218f
Dallas Norton <dallaslandonnorton@gmail.com>
2014-07-08 00:28:14 -0600

    cleaning up trying to get http server

--- server.js
+++ server.js
@@ -1,29 +1,44 @@
+// var express = require('express');
+// var fs = require('fs');
+
+// app = express();
+// STATIC_DIR = __dirname + '/public';
+
+// var options = {
+//     key: fs.readFileSync('./ssl/key.pem'),
+//     cert: fs.readFileSync('./ssl/cert.pem')
+// };
+
+// serverhttps = require('https')
+//     .createServer(options, app);
+
+// // app.configure(function() {
+// //     app.use(express.static(STATIC_DIR));
+// //     app.use(express.bodyParser());
+// // });
+
+
+// // Redirect http(80) to https(443)
+// // http = express();
+// // http.get('*', function(req, res) {
+// //     // res.redirect('https://tharris7.lab.novell.com' + req.url)
+// // });
+// // http.listen(80);
+
+// serverhttps.listen(443);
+// console.log(new Date() + ' Express server is listening securely on port ' + '443');
+
 var express = require('express');
 var fs = require('fs');
 
-app = express();
-STATIC_DIR = __dirname + '/public';
+app = express();   
 
 var options = {
-    key: fs.readFileSync('./ssl/key.pem'),
-    cert: fs.readFileSync('./ssl/cert.pem')
+    key: fs.readFileSync('./ssl/key.pem'),
+    cert: fs.readFileSync('./ssl/cert.pem')
 };
 
-serverhttps = require('https')
-    .createServer(options, app);
-
-app.configure(function() {
-    app.use(express.static(STATIC_DIR));
-    app.use(express.bodyParser());
-});
-
-
-// Redirect http(80) to https(443)
-http = express();
-http.get('*', function(req, res) {
-    res.redirect('https://tharris7.lab.novell.com' + req.url)
-});
-http.listen(80);
+serverhttps = require('https').createServer(options, app);
 
 serverhttps.listen(443);
 console.log(new Date() + ' Express server is listening securely on port ' + '443');
