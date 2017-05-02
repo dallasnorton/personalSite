8851c04a9d7b278f5aa8658660408fb3537e9c5b
dallasnorton <dallaslandonnorton@gmail.com>
2017-05-01 23:08:25 -0600

    styling scaffolding

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
           <h2>Dallas Norton</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload. yo
+          My content yo
         </p>
       </div>
     );
