23d402ae21f5f3129614ee7d6ea06a5c4e9acf93
dallasnorton <dallaslandonnorton@gmail.com>
2017-05-09 21:56:53 -0600

    additional article completed

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import Articles from './Articles';
 
 class App extends Component {
   render() {
-    const articles = [ {
-        title: 'AngularJS: `template` vs `templateUrl`',
-        href: 'https://medium.com/@frosty/angularjs-template-vs-templateurl-cdde055b7907',
-        takeaways: 'I guess I never really thought about the issue of making requests for templates using templateURL or using a build tool. On the personal projects I have worked on they have been small enough that making additional network requests for templates hasn\'t been a problem and also by having a large bundle. It\'s great to work on such a large application to get exposure to different types of problems.',
-        author: 'Aaron Frost',
-        authorLink: 'https://twitter.com/js_dev',
-        date: new Date('May 9, 2017')
+    const articles = [{
+        "title": "AngularJS: `template` vs `templateUrl`",
+        "href": "https://medium.com/@frosty/angularjs-template-vs-templateurl-cdde055b7907",
+        "takeaways": "I guess I never really thought about the issue of making requests for templates using templateURL or using a build tool. On the personal projects I have worked on they have been small enough that making additional network requests for templates hasn't been a problem and also by having a large bundle. It's great to work on such a large application to get exposure to different types of problems.",
+        "author": "Aaron Frost",
+        "authorLink": "https://twitter.com/js_dev",
+        "date": "May 9, 2017"
       }, {
-        title: 'There\'s more ot HTML escaping than &, <, >, and "',
-        href: 'http://wonko.com/post/html-escaping',
-        takeaways: 'Read this article to tighten security around a feature I inherited that used lodash _.escape but that can even fall to XSS in the case of the all the extra special characters like `, , !, @, $, %, (, ), =, +, {, }, [, and ]. One of the key takeaways is context is key. In which context am I going to be using my users input? Ryan Grove also links a few other references that I will need to go back to when I get more time.',
-        author: 'Ryan Grove',
-        authorLink: 'https://twitter.com/yaypie',
-        date: new Date('May 1, 2017')
+        "title": "There's more ot HTML escaping than &, <, >, and \"",
+        "href": "http://wonko.com/post/html-escaping",
+        "takeaways": "Read this article to tighten security around a feature I inherited that used lodash _.escape but that can even fall to XSS in the case of the all the extra special characters like `, , !, @, $, %, (, ), =, +, {, }, [, and ]. One of the key takeaways is context is key. In which context am I going to be using my users input? Ryan Grove also links a few other references that I will need to go back to when I get more time.",
+        "author": "Ryan Grove",
+        "authorLink": "https://twitter.com/yaypie",
+        "date": "May 1, 2017"
       }];
     
     return (
@@ -33,11 +33,6 @@ class App extends Component {
     );
   }
 }
-
-
-// function Article(props) {
-//   return <div>Article #{props.num}</div>;
-// }
 
 // This website is to help me keep up. There is so much going 
 // on on the web that its hard to know what you don’t know. 
