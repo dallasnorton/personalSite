90884e7f403969da302d9147b8a64e1c9a034514
dallasnorton <dallaslandonnorton@gmail.com>
2017-05-09 22:07:54 -0600

    Confucius quote

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ class App extends Component {
         </div>
         <div className="main-wrapper">
           <div className="content-wrapper">
-            <div className="App-intro">Welcome, I use this website to keep a history of articles I have recently read and will make note of key points and/or things I have learned from said article.</div>
+            <div className="App-intro">Welcome, I use this website to keep a history of articles I have recently read and will make note of key points and/or things I have learned from said article. In the words of Confucius “Real knowledge is to know the extent of one’s ignorance”</div>
             <Articles className="article-container"   articles={articles}/>
           </div>
         </div>
