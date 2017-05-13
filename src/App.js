62aa8938235314b28242b46f21ad53d70fbf2c6a
dallasnorton <dallaslandonnorton@gmail.com>
2017-05-12 21:39:06 -0600

    split header into component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import articlesData from './articlesData';
 import './App.css';
 import Articles from './Articles/Articles';
 import RightSideBar from './RightSideBar/RightSideBar';
+import AppHeader from './AppHeader/AppHeader';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h1 className="logo">DALLAS NORTON</h1>
-        </div>
+        <AppHeader/>
         <div className="main-wrapper">
           <div className="content-wrapper">
             <Articles articles={articlesData.articles}/>
---
+++ src/AppHeader/AppHeader.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './AppHeader.css';
+
+class AppHeader extends Component {
+  render() {
+    return <div className="app-header">
+            <h1 className="logo">DALLAS NORTON</h1>
+          </div>;
+  }
+}
+
+export default AppHeader;
