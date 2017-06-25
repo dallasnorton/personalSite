76270dd4f5903490513fcff712dab97fdea0bcaa
dallasnorton <dallaslandonnorton@gmail.com>
2017-06-24 21:43:49 -0600

    Google Analytics

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import App from './App';
 import './index.css';
 import './cayman.css';
 import registerServiceWorker from './registerServiceWorker.js';
+import ReactGA from 'react-ga';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
 registerServiceWorker();
+
+ReactGA.initialize('UA-101588969-1', {});
+ReactGA.pageview('home');
